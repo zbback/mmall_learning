db0b4882135d997e81133a20b5009d176a7133b9
wangzb <524442630@qq.com>
2019-08-23 16:06:39 +0800

    revert add conts2

--- src/main/java/com/mmall/common/Conts2.java
+++ src/main/java/com/mmall/common/Conts2.java
@@ -1,20 +1,20 @@
 package com.mmall.common;
 
 public class Conts2 {
-    public enum PersonImfo{
-        LIKENAN(1,"男的"),
-        LIKEWOMAN(2,"女的");
-
-        private String value;
-        private int code;
-
-        PersonImfo(int code,String value){
-            this.code = code;
-            this.value = value;
-        }
-
-        public String getValue(){return value;}
-        public int getCode(){return code;}
-
-    }
+//    public enum PersonImfo{
+//        LIKENAN(1,"男的"),
+//        LIKEWOMAN(2,"女的");
+//
+//        private String value;
+//        private int code;
+//
+//        PersonImfo(int code,String value){
+//            this.code = code;
+//            this.value = value;
+//        }
+//
+//        public String getValue(){return value;}
+//        public int getCode(){return code;}
+//
+//    }
 }
